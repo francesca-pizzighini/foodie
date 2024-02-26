67a17a90402c1d9d27ca1cecfe510a0152a22638
francesca-pizzighini <133757414+francesca-pizzighini@users.noreply.github.com>
2024-02-26 12:16:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>sito ricette</p>
+    </>
+  )
+}
+
+export default App
