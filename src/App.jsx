ab610e800342956cf6523b6ee78da325051d7c06
francesca-pizzighini <133757414+francesca-pizzighini@users.noreply.github.com>
2024-02-26 18:50:00 +0100

    navbar working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.scss'
 
 import Navbar from './components/navbar.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,62 @@
 import { NavLink } from "react-router-dom"
 
+import foodie from "../assets/img/foodie.png"
+
 function Navbar() {
+  const x=15
+
+  const recipes = document.getElementById("recipes")
+  const about = document.getElementById("about")
+  const home = document.getElementById("logo")
+  const menuDisplay = ({ isActive }) => {
+    if (window.innerWidth > 600) {
+      return {
+        display: "block"
+      }
+    }else if (window.innerWidth < 600) {
+      return {
+        display: isActive ? "none" : "block"
+      }
+    }
+  }
+  const logoDisplay = {
+    height: window.innerWidth > 600 ? "70px" : "50px",
+  }
+    
+  
 
   return (
-    <nav>
-        <NavLink to={"/"}>Home</NavLink>
-        <NavLink to={"/recipes"}>Recipes</NavLink>
-        <NavLink to={"/about"}>About the project</NavLink>
+    <nav className="navbar">
+        <NavLink 
+        style={logoDisplay}
+          className="satisfy-regular" 
+          id="logo" 
+          to={"/"}
+          >
+            <img 
+              style={logoDisplay}
+              src={foodie} 
+              alt="logo del brand"
+            />
+        </NavLink>
+
+        <NavLink 
+          style={menuDisplay}
+          className="satisfy-regular" 
+          id="recipes" 
+          to={"/recipes"}
+          >
+            Recipes
+        </NavLink>
+
+        <NavLink 
+          style={menuDisplay}
+          className="satisfy-regular" 
+          id="about" 
+          to={"/about"}
+          >
+            About{window.innerWidth > 600 ? " the project" : null}
+          </NavLink>
     </nav>
   )
 }
