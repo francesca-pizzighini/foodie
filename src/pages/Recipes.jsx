4232d7ed414aa1076d02ef765d793b287985367e
francesca-pizzighini <133757414+francesca-pizzighini@users.noreply.github.com>
2024-03-08 15:34:52 +0100

    dev branch prova

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -79,6 +79,7 @@ function Recipes() {
     (async () => {
       try{
         setError(false);
+        console.log('dev prova')
 
         const response = await axios.get(url);
         const data = await response.data.results;
